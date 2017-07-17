7b60a597151b6a3c0f98c26f3eaee2c83a93457d
ben awad <benawad97@gmail.com>
2017-07-16 19:05:49 -0500

    grab tempalte

---
+++ models/user.js
@@ -0,0 +1,25 @@
+export default (sequelize, DataTypes) => {
+  const User = sequelize.define('User', {
+    username: {
+      type: DataTypes.STRING,
+      unique: true,
+    },
+    isAdmin: {
+      type: DataTypes.BOOLEAN,
+      defaultValue: false,
+    },
+  });
+
+  User.associate = (models) => {
+    // 1 to many with board
+    User.hasMany(models.Board, {
+      foreignKey: 'owner',
+    });
+    // 1 to many with suggestion
+    User.hasMany(models.Suggestion, {
+      foreignKey: 'creatorId',
+    });
+  };
+
+  return User;
+};
